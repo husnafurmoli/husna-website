45cbe570ac62c4a1a7b4305742bd159ef2857529
husnafurmoli <husnafurmoli@gmail.com>
2026-04-26 17:33:43 -0700

    Restructure homepage to include core hypnotherapy sections

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
 import { Hero } from '../components/Hero';
+import { Benefits } from '../components/Benefits';
+import { Specialties } from '../components/Specialties';
+import { About } from '../components/About';
+import { FAQ } from '../components/FAQ';
 import { Testimonials } from '../components/Testimonials';
 
 export function HomePage() {
   return (
     <div className="space-y-20 lg:space-y-24 pb-20">
       <Hero />
+      <Benefits />
+      <Specialties />
+      <About />
+      <FAQ />
       <Testimonials />
     </div>
   );
